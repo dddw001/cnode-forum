13443bd0cef13dfaa4e1f604dc2b0dbd8e218dfb
dddw001 <2410656679@qq.com>
2018-12-01 23:58:44 +0800

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <p>
+      {{count}}
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'hi'
+    }
+  },
+  computed: {
+    count () {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      this.$store.commit('increment')
+    },
+    decrement () {
+      this.$store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.hello {
+  h2 {
+    color: red;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuex from 'vuex'
+import store from './store/store'
+
+Vue.config.productionTip = false
+
+Vue.use(Vuex)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  router,
+  components: { App },
+  template: '<App/>'
+})
